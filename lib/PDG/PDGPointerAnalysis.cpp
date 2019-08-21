4ddcd77faf4e5ecfdedc35892cbdfac6edd9916b
anahitH <anahithayrapetyan91@gmail.com>
2019-08-21 22:12:56 +0000

    Fixed cmake issues

--- lib/PDG/PDGPointerAnalysis.cpp
+++ lib/PDG/PDGPointerAnalysis.cpp
@@ -4,9 +4,14 @@
 #include "PDG/PDGBuilder.h"
 
 #include "Util/ThreadCallGraph.h"
-#include "SVF/Util/SVFModule.h"
-#include "SVF/MemoryModel/CHA.h"
-#include "SVF/MemoryModel/PTAType.h"
+#include "Util/SVFModule.h"
+#include "MemoryModel/CHA.h"
+#include "MemoryModel/PTAType.h"
+
+
+//#include "SVF/Util/SVFModule.h"
+//#include "SVF/MemoryModel/CHA.h"
+//#include "SVF/MemoryModel/PTAType.h"
 
 namespace svfg {
 
